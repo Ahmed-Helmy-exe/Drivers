d44c397e61be6c3c05c4153d8f4024d5372531d9
Amed-Helmy-exe <Ahmed.Helmy.Engineer@gmail.com>
2022-02-09 16:00:38 +0200

    MY CODES

---
+++ TEMP.h
@@ -0,0 +1,19 @@
+﻿/*
+ * TEMP.h
+ *
+ * Created: 09/02/2022 12:52:43 م
+ *  Author: Ahmed Helmy
+ */ 
+
+
+#ifndef TEMP_H_
+#define TEMP_H_
+
+#include "ADC.h"
+
+void Temp_sensor_init(void);
+
+u16 Temp_sensor_read(void);
+
+
+#endif /* TEMP_H_ */
---
+++ TEMP_CFG.h
@@ -0,0 +1,16 @@
+﻿/*
+ * TEMP_CFG.h
+ *
+ * Created: 09/02/2022 12:52:54 م
+ *  Author: Ahmed Helmy
+ */ 
+
+
+#ifndef TEMP_CFG_H_
+#define TEMP_CFG_H_
+
+
+
+
+
+#endif /* TEMP_CFG_H_ */
